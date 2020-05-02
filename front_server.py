782f93207ae79a2fe5fbdabb28d82a37d290f24c
student <chhatbarraj@csu.fullerton.edu>
2020-05-02 08:29:51 -0700

    rss now working

--- front_server.py
+++ front_server.py
@@ -1,8 +1,7 @@
-import datetime, math
-import json
-from flask import Flask, jsonify, request, send_from_directory
-import requests
-
+import math, json, requests
+from flask import Flask, jsonify, request, send_from_directory,make_response
+from rfeed import *
+from datetime import datetime
 
 # flask globals
 app = Flask(__name__)
@@ -11,12 +10,20 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
+# static values
+POST_PORT_NO=5100 #set port number for post_api
+VOTE_PORT_NO=5200 #set port number for vote_api
+
+
+# n not found error
+def custom_error(message, status_code):
+    return make_response(jsonify(message), status_code)
+
 # 404 page
 @app.errorhandler(404)
 def page_not_found(status_code=404):
     error_json = get_response(status_code=status_code, message="Resource not found")
     return jsonify(error_json), status_code
-
 
 # fix favicon 500 error (Reference used)
 @app.route('/favicon.ico')
@@ -27,20 +34,24 @@ def favicon():
 def get_response(status_code, message):
     return {"status_code": str(status_code), "message": str(message)}
 
+
+
+
 class APIError(Exception):
-    """An API Error Exception"""
+    """throws API error exception"""
 
     def __init__(self, status):
         self.status = status
 
     def __str__(self):
-        return "APIError: status={}".format(self.status)
+        return {"APIError: status": str(self.status)}
+
+
 
 # 1) The 25 most recent posts to a particular community
 # http://localhost:5000/get?n=25&community_name=csuf
 #2) The 25 most recent posts to any community
 #http://localhost:5000/get?n=25
-
 @app.route('/get', methods=["GET"])
 def get_recent_post():
     """
@@ -59,25 +70,50 @@ def get_recent_post():
 
         if params.get('community_name') is not None:
             community_name=str(params['community_name'])
-            resp = requests.get('http://127.0.0.1:5100/get?n={}&community_name={}&recent=True'.format(no_of_post,community_name))
+            post_resp = requests.get('http://127.0.0.1:{}/get?n={}&community_name={}&recent=True'.format(POST_PORT_NO,no_of_post,community_name))
         else:
-            resp = requests.get('http://127.0.0.1:5100/get?n={}&recent=True'.format(no_of_post))
+            post_resp = requests.get('http://127.0.0.1:{}/get?n={}&recent=True'.format(POST_PORT_NO,no_of_post))
 
-        if resp.status_code != 200:
+        if post_resp.status_code != 200:
         # This means something went wrong.
-            raise APIError(resp.status_code)
+            raise APIError(post_resp.status_code)
+        data=post_resp.json() # convert fetch data to python list of dict
 
-        data=resp.json() # convert fetch data to python dict
+        # generate RSS
+        rss_list=[]
+        for dic in data:
+            date = datetime.fromtimestamp(int(dic['published']))
+
+            item1 = Item(
+                title = dic.get('title',"") or "", # get title form dict or empty string
+                link = dic.get('url',"") or "",
+                description = dic.get('description',"") or "",
+                author = dic.get('username',"") or "",
+                guid = Guid(dic.get('url',"") or ""),
+                categories= dic.get('community_name',"") or "",
+                pubDate = date)
+            rss_list.append(item1)
+
+        feed = Feed(
+            title = "Reddit clone RSS Feed",
+            link = "http://www.example.com/rss",
+            description = "This is project-2 for CPSC-449 an RSS 2.0 feed",
+            language = "en-US",
+            lastBuildDate = datetime.now(),
+            items = rss_list)
+
+        response = make_response(feed.rss())
+        response.headers.set('Content-Type', 'application/rss+xml')
+        return response
 
 
-        return jsonify(data), 200
     else:
-        return jsonify(get_response(404, "n not found"))
+        return custom_error("Enter value of n",404)
 
 
-# The top 25 posts to a particular community, sorted by score
-# The top 25 posts to any community, sorted by score
+# 3) The top 25 posts to a particular community, sorted by score
 # http://localhost:5000/get_sorted?n=25&community_name=csuf
+# 4) The top 25 posts to any community, sorted by score
 # http://localhost:5000/get_sorted?n=25
 @app.route('/get_sorted', methods=["GET"])
 def get_recent_post_scorted():
@@ -94,10 +130,10 @@ def get_recent_post_scorted():
         # got number of post, now fetching data using votes_api
         if params.get('community_name') is not None:
             community_name=str(params['community_name'])
-            vote_resp = requests.get('http://127.0.0.1:5200/get?n={}&community_name={}&sorted=True'.format(no_of_post,community_name))
+            vote_resp = requests.get('http://127.0.0.1:{}/get?n={}&community_name={}&sorted=True'.format(VOTE_PORT_NO,no_of_post,community_name))
 
         else:
-            vote_resp = requests.get('http://127.0.0.1:5200/get?n={}&sorted=True'.format(no_of_post))
+            vote_resp = requests.get('http://127.0.0.1:{}/get?n={}&sorted=True'.format(VOTE_PORT_NO,no_of_post))
 
         if vote_resp.status_code != 200:
             # This means something went wrong.
@@ -111,18 +147,42 @@ def get_recent_post_scorted():
         # use these uuids to retrieve posts from post_api
         data={"uuid": vote_scoted_list}
         header = {"Content-type": "application/json",}
-        post_resp = requests.post('http://localhost:5100/get_uuids', data=json.dumps(data), headers=header)
+        post_resp = requests.post('http://localhost:{}/get_uuids'.format(POST_PORT_NO), data=json.dumps(data), headers=header)
 
         dict_post_resp = (post_resp.json())
-        return jsonify(dict_post_resp), 200
 
+        # generate RSS
+        rss_list=[]
+        for dic in dict_post_resp:
+            date = datetime.fromtimestamp(int(dic['published']))
 
+            item1 = Item(
+                title = dic.get('title',"") or "", # get title form dict or empty string
+                link = dic.get('url',"") or "",
+                description = dic.get('description',"") or "",
+                author = dic.get('username',"") or "",
+                guid = Guid(dic.get('url',"") or ""),
+                categories= dic.get('community_name',"") or "",
+                pubDate = date)
+            rss_list.append(item1)
+
+        feed = Feed(
+            title = "Reddit clone RSS Feed",
+            link = "http://www.example.com/rss",
+            description = "This is project-2 for CPSC-449 an RSS 2.0 feed",
+            language = "en-US",
+            lastBuildDate = datetime.now(),
+            items = rss_list)
+
+        response = make_response(feed.rss())
+        response.headers.set('Content-Type', 'application/rss+xml')
+        return response
     else:
-        return jsonify(get_response(404, "n not found"))
+        return custom_error("Enter value of n",404)
 
 
 
-#The hot 25 posts to any community, ranked using Reddit’s “hot ranking” algorithm.
+#5) The hot 25 posts to any community, ranked using Reddit’s “hot ranking” algorithm.
 # http://localhost:5000/get_hot?n=25
 def hot(score, date):
     """
@@ -138,12 +198,17 @@ def hot(score, date):
 
 @app.route('/get_hot', methods=["GET"])
 def get_hot_post():
+    """
+        This route takes ONE arguments
+        n: Number of Posts (mandatory or required)
+
+    """
     params = request.args
     if params.get('n') is not None:
         no_of_post=int(params['n'])
 
         # got number of post, now fetching data using votes_api
-        vote_resp = requests.get('http://127.0.0.1:5200/get_all')
+        vote_resp = requests.get('http://127.0.0.1:{}/get_all'.format(VOTE_PORT_NO))
         if vote_resp.status_code != 200:
             # This means something went wrong.
             raise APIError(vote_resp.status_code)
@@ -162,13 +227,39 @@ def get_hot_post():
 
         data={"uuid": uuid_list_to_post}
         header = {"Content-type": "application/json",}
-        post_resp = requests.post('http://localhost:5100/get_uuids', data=json.dumps(data), headers=header)
+        post_resp = requests.post('http://localhost:{}/get_uuids'.format(POST_PORT_NO), data=json.dumps(data), headers=header)
 
         dict_post_resp = (post_resp.json())
-        return jsonify(dict_post_resp), 200
+
+        # generate RSS
+        rss_list=[]
+        for dic in dict_post_resp:
+            date = datetime.fromtimestamp(int(dic['published']))
+
+            item1 = Item(
+                title = dic.get('title',"") or "", # get title form dict or empty string
+                link = dic.get('url',"") or "",
+                description = dic.get('description',"") or "",
+                author = dic.get('username',"") or "",
+                guid = Guid(dic.get('url',"") or ""),
+                categories= dic.get('community_name',"") or "",
+                pubDate = date)
+            rss_list.append(item1)
+
+        feed = Feed(
+            title = "Reddit clone RSS Feed",
+            link = "http://www.example.com/rss",
+            description = "This is project-2 for CPSC-449 an RSS 2.0 feed",
+            language = "en-US",
+            lastBuildDate = datetime.now(),
+            items = rss_list)
+
+        response = make_response(feed.rss())
+        response.headers.set('Content-Type', 'application/rss+xml')
+        return response
 
     else:
-        return jsonify(get_response(404, "n not found"))
+        return custom_error("Enter value of n",404)
 
 
 
